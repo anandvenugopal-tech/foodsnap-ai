b9e28e38654ed27fbddd21e71c1929622607b973
anand <anand.venugopal00@gmail.com>
2026-02-24 14:11:16 +0530

    add database

---
+++ database/db.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_PASSWORD =", os.getenv("DB_PASSWORD"))
+print("DB_NAME =", os.getenv("DB_NAME"))
